d08ebded3114b157b84a0250c046403fa192db34
ahlam aldadi <ahlam_aldadi@hotmail.com>
2018-10-20 10:53:27 +0300

    first commit

---
+++ udacity1.py
@@ -0,0 +1,16 @@
+class Udacian:
+    def __init__(self,Udacian_name,Udacian_city,Udacian_enrollment,Udacian_nanodegree,Udacian_status):
+        self.name=Udacian_name
+        self.city=Udacian_city
+        self.enrollment=Udacian_enrollment
+        self.nanodegree=Udacian_nanodegree
+        self.status=Udacian_status
+    def Print(self):
+        print "name:"+self.name+"  city:"+self.city+"  enrollment:"+self.enrollment+"  nanodegree:" +self.nanodegree+ "  status:"+self.status
+
+
+
+
+user = Udacian("Ahlam","Makkah","computer","full stack web developer","student")
+
+user.Print()
